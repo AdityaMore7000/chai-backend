f931d721ec71305f52a8784492ede3bbddf7a020
AdityaMore7000 <adi.more7000@gmail.com>
2024-10-25 15:02:06 +0530

    added async wrapper and error utils

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import 'dotenv/config'
 
 import connectDB from "./db/index.js";
-
-connectDB();
+import { app } from './app.js';
+const port = process.env.PORT||3000
+connectDB()
+.then(()=>{
+    app.on("error",(err)=>{
+        console.error("Error! :",err)
+    })
+    app.listen(port,()=>{
+        console.log(`Alive on http://localhost:${port}`)
+    })
+})
+.catch((err)=>{
+    console.error("Mongo Connection Failed! ",err)
+})
